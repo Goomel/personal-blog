ef938e910bf4c09054e6cba8f55dccf6f3e427cf
Jakub Grzymisławski <thegomel10@gmail.com>
2024-03-01 23:30:29 +0100

    The first succesful post rendering with metadata

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,11 +1,32 @@
 import fs from 'fs';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import path from 'path';
+import { compileMDX } from 'next-mdx-remote/rsc';
 
-// const blogPost = fs.readFileSync('src/posts/test-1.mdx');
-// console.log('blogPost', blogPost);
+const pathToPosts = path.join(process.cwd(), 'src/posts');
 
-export default async function RemoteMdxPage({ params }) {
-  const blogPost = fs.readFileSync(`src/posts/${params.slug}.mdx`);
-  const markdown = await blogPost;
-  return <MDXRemote source={markdown} />;
+export async function generateMetadata({ params }) {
+  const pathToFile = path.join(pathToPosts, `${params.slug}.mdx`);
+  const source = fs.readFileSync(pathToFile, 'utf-8');
+
+  const { content, frontmatter } = await compileMDX({
+    source: source,
+    options: { parseFrontmatter: true }
+  });
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.description
+  };
+}
+
+export default async function BlogPage({ params }) {
+  const pathToFile = path.join(pathToPosts, `${params.slug}.mdx`);
+  const source = fs.readFileSync(pathToFile, 'utf-8');
+
+  const { content, frontmatter } = await compileMDX({
+    source: source,
+    options: { parseFrontmatter: true }
+  });
+
+  return <>{content}</>;
 }
