f995b2f402c18dd4b7e729ed4897c3b65b8ef98b
Jakub Grzymisławski <thegomel10@gmail.com>
2024-03-23 00:07:08 +0100

    Fix bug with mobile menu on resize

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,20 +1,23 @@
 'use client';
 
 import { useState } from 'react';
+import { useMediaQuery } from '@uidotdev/usehooks';
 import Logo from '@/components/navigation/logo/Logo';
 import Hamburger from './hamburger/Hamburger';
 import ListWrapper from './listWrapper/ListWrapper';
 import SocialLinks from './socialLinks/SocialLinks';
 import styles from './navigation.module.scss';
-// import { useMedia } from 'react-use';
 
 const Navigation = () => {
-  // useMedia hook needs the second argument set to true to avoid hydration problems
-  // const isMobile = useMedia('(max-width: 768px)', true);
+  const isMobileDevice = useMediaQuery('(max-width: 768px)');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenuOpen = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  if (isMenuOpen && !isMobileDevice) {
+    setIsMenuOpen(false);
+  }
 
   return (
     <nav className={styles.navigationWrapper}>
