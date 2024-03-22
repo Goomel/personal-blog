5a1b61f84ae610eb695596d34a32c15c1173343a
Jakub Grzymisławski <thegomel10@gmail.com>
2024-03-22 23:39:16 +0100

    Hamburger menu

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,11 +6,11 @@ import Hamburger from './hamburger/Hamburger';
 import ListWrapper from './listWrapper/ListWrapper';
 import SocialLinks from './socialLinks/SocialLinks';
 import styles from './navigation.module.scss';
-import { useMedia } from 'react-use';
+// import { useMedia } from 'react-use';
 
 const Navigation = () => {
-  // useMedia hook needs second argument set to true to avoid hydration problems
-  const isMobile = useMedia('(max-width: 1024px)', true);
+  // useMedia hook needs the second argument set to true to avoid hydration problems
+  // const isMobile = useMedia('(max-width: 768px)', true);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenuOpen = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -19,14 +19,13 @@ const Navigation = () => {
   return (
     <nav className={styles.navigationWrapper}>
       <Logo />
-      {isMobile ? (
-        <Hamburger isOpen={isMenuOpen} handleClick={toggleMenuOpen} />
-      ) : (
-        <div className={styles.innerWrapper}>
-          <ListWrapper />
-          <SocialLinks />
-        </div>
-      )}
+      <Hamburger isOpen={isMenuOpen} handleClick={toggleMenuOpen} />
+      <div
+        className={`${styles.innerWrapper} ${isMenuOpen ? styles.active : ''}`}
+      >
+        <ListWrapper />
+        <SocialLinks />
+      </div>
     </nav>
   );
 };
