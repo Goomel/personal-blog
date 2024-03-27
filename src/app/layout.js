4c5ea76471c8791fa589e2c1e545c110a31a7e45
Jakub Grzymisławski <thegomel10@gmail.com>
2024-03-27 22:23:49 +0100

    Fix bug with useMediaQuery hook

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
-import { Work_Sans, Anton } from 'next/font/google';
 import cn from 'classnames';
+import dynamic from 'next/dynamic';
+import { Work_Sans, Anton } from 'next/font/google';
 import styles from './layout.module.scss';
+import '@/styles/main.scss';
+
+const Navigation = dynamic(() => import('@/components/navigation/Navigation'), {
+  ssr: false
+});
 
 const work_sans = Work_Sans({
   subsets: ['latin'],
@@ -12,11 +18,8 @@ const anton = Anton({
   subsets: ['latin'],
   display: 'swap',
   weight: '400',
-  variable: '--font-anton'
+  variable: '--font-heading'
 });
-
-import Navigation from '@/components/navigation/Navigation';
-import '@/styles/main.scss';
 
 export const metadata = {
   title: 'Create Next App',
